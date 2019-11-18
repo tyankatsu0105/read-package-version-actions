471407a29477f54c4f07b3d1686791f0a12a49f5
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2019-11-18 15:35:19 +0900

    feat: create main function

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,36 @@
 import * as core from '@actions/core';
-import { wait } from './wait';
+import { getPackageVersion } from './getPackageVersion';
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`);
+    const path = core.getInput('path');
 
-    core.debug(new Date().toTimeString());
-    await wait(parseInt(ms, 10));
-    core.debug(new Date().toTimeString());
+    core.debug(`Load package.json at ${path}`);
 
-    core.setOutput('time', new Date().toTimeString());
+    const result = getPackageVersion(path);
+    core.setOutput('version', result);
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
 run();
+
+// import * as core from '@actions/core';
+
+// async function run() {
+//   try {
+//     const ms = core.getInput('milliseconds');
+//     console.log(`Waiting ${ms} milliseconds ...`);
+
+//     core.debug(new Date().toTimeString());
+//     await wait(parseInt(ms, 10));
+//     core.debug(new Date().toTimeString());
+
+//     core.setOutput('time', new Date().toTimeString());
+//   } catch (error) {
+//     core.setFailed(error.message);
+//   }
+// }
+
+// run();
